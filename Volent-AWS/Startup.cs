fe2536a1fe99b5627e5aeb084b9e3ad9e0209990
hasangas <hasangas@irononetech.com>
2022-01-24 00:10:32 +0530

    modified project to AWS serverless

--- Volent-AWS/Startup.cs
+++ Volent-AWS/Startup.cs
@@ -4,38 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Volent.Data;
 
-namespace Volent
+namespace Volent_AWS
 {
     public class Startup
     {
-
-        public IConfiguration Configuration { get; }
-
         public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder();
-            configuration = builder.Build();
             Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public static IConfiguration Configuration { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var connectionString = "";
-            services.AddDbContext<DBContext>(c => { c.UseMySql(connectionString); });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -52,6 +46,10 @@ namespace Volent
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                });
             });
         }
     }
